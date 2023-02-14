7571a3bc2aea48cd9e9098f176447304d5676684
Toggly <toggledy@gmail.com>
2023-02-15 01:54:49 +1100

    Don't gitignore dist for libraries

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   plugins: [vue()],
-
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'lib/defineState.ts'),
+      entry: resolve(__dirname, './lib/defineState.ts'),
       name: 'vue-define-state',
-      // the proper extensions will be added
       fileName: 'vue-define-state',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: 'Vue',
         },
