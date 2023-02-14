60b5781025266303f63d4c447a51db3e2bee4f54
Toggly <toggledy@gmail.com>
2023-02-15 02:00:07 +1100

    Add dts plugin to generate type defs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, './lib/defineState.ts'),
