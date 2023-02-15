22d4d3fcbab4a064dd75fa5b4c3af105c62bf4e3
Toggly <toggledy@gmail.com>
2023-02-16 03:22:42 +1100

    0.0.2

--- dist/vue-define-state.d.ts
+++
@@ -1,9 +0,0 @@
-import { UnwrapNestedRefs } from "vue";
-/**
- * A convenient wrapper for Vue's `reactive()`.
- * Creates a reactive copy of the original object, and also:
- *
- * - Makes all getters into computed values
- * - Binds "this" for all non-getter functions
- */
-export declare function defineState<T extends object>(obj: T): UnwrapNestedRefs<T>;
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/guide/build.html#library-mode
 export default defineConfig({
   plugins: [
     vue(),
-    dts({
-      beforeWriteFile: (filePath, content) => {
-        return {
-          filePath: 'dist/vue-define-state.d.ts',
-        }
-      }
-    })
   ],
-  build: {
-    sourcemap: true,
-    lib: {
-      entry: resolve(__dirname, './src/defineState.ts'),
-      name: 'VueDefineState',
-      fileName: 'vue-define-state',
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
 })
